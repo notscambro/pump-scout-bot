fd9bd7a573c00aac35fef0a0756ed34e4782f287
notscambro <said.hasanpor1998@gmail.com>
2026-06-15 10:54:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,10 @@
-import requests
-import time
+from dexscreener import DexscreenerClient
 
-def get_pairs():
-    url = "https://api.dexscreener.com/latest/dex/pairs"
+client = DexscreenerClient()
 
-    r = requests.get(url)
+print("Bot Started")
 
-    print("STATUS:", r.status_code)
-    print("TEXT:", r.text[:500])
+pairs = client.search_pairs("SOL")
 
-    return []
-
-
-def score(pair):
-    liquidity = pair.get("liquidity", {}).get("usd", 0)
-    volume = pair.get("volume", {}).get("h24", 0)
-    change = pair.get("priceChange", {}).get("h24", 0)
-
-    score = 0
-
-    if liquidity > 50000:
-        score += 30
-    elif liquidity > 10000:
-        score += 15
-
-    if volume > 100000:
-        score += 30
-    elif volume > 20000:
-        score += 15
-
-    if change > 20:
-        score += 25
-    elif change > 5:
-        score += 10
-
-    return score
-
-
-def run():
-    pairs = get_pairs()
-    results = []
-
-    for p in pairs[:50]:
-        try:
-            s = score(p)
-
-            results.append({
-                "name": p.get("baseToken", {}).get("name"),
-                "symbol": p.get("baseToken", {}).get("symbol"),
-                "score": s,
-                "liquidity": p.get("liquidity", {}).get("usd", 0),
-                "volume": p.get("volume", {}).get("h24", 0),
-                "change": p.get("priceChange", {}).get("h24", 0),
-                "url": p.get("url")
-            })
-        except:
-            pass
-
-    results.sort(key=lambda x: x["score"], reverse=True)
-
-    for r in results[:10]:
-        print(r)
-
-
-if __name__ == "__main__":
-    while True:
-        print("\n🔍 scanning market...\n")
-        run()
-        time.sleep(300)
+for pair in pairs[:5]:
+    print(pair.base_token.name)
